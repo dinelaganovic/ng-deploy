9e011851357b98a37f166c90fbb7724a95bad6dc
dinela <dinelaganovic99@gmail.com>
2024-05-11 23:28:22 +0200

    change responsive...

--- src/app/portfolio/portfolio-routing.module.ts
+++ src/app/portfolio/portfolio-routing.module.ts
@@ -5,6 +5,7 @@ import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { SkillsComponent } from './skills/skills.component';
 
 const routes: Routes = [
   {path:'navbar',component:NavbarComponent},
@@ -12,6 +13,8 @@ const routes: Routes = [
   {path:'home',component:HomeComponent},
   {path:'about',component:AboutComponent},
   {path:'contact',component:ContactComponent},
+  {path:'skills',component:SkillsComponent},
+
 ];
 
 @NgModule({
